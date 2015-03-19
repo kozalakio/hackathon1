ce9d4bd811128534c27041be3a2d0bdf7ea10b2f
Murat Sutunc <muratsu@microsoft.com>
2015-03-19 14:09:15 -0700

    express helloworld

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ *  Module dependencies
+ */
+
+var express = require('express');
+
+
+/**
+ * Create express server
+ */
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
